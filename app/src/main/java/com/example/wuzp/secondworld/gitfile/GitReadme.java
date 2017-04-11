6f6f35ad0e5e8916e9dfdb63fbb8f10400e6c9a2
wzp09tjlg <wzp09tjlg@163.com>
2017-04-11 16:11:03 +0800

    1.save file to dev branch,and commit to origin

--- app/src/main/java/com/example/wuzp/secondworld/gitfile/GitReadme.java
+++ app/src/main/java/com/example/wuzp/secondworld/gitfile/GitReadme.java
@@ -1,8 +1,19 @@
 package com.example.wuzp.secondworld.gitfile;
 
+import com.apkfuns.logutils.LogUtils;
+
 /**
  * Created by wuzp on 2017/4/11.
  */
-
 public class GitReadme {
+
+    //this is method just to log msg to console
+    public static void show(String msg){
+        LogUtils.e("this is a show:" + msg);
+    }
+
+    //this method is carry a tag to console to defer different kind msg
+    public static void show(String tag,String msg){
+        LogUtils.e(tag,msg);
+    }
 }
