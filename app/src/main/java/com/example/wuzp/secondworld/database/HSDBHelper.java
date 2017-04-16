e2631fdc8126ae03365cfe94949a820b2bfc8dc2
wzp09tjlg <wzp09tjlg@163.com>
2017-04-16 20:17:35 +0800

    1.add some file of dbhelper

---
+++ app/src/main/java/com/example/wuzp/secondworld/database/HSDBHelper.java
@@ -0,0 +1,19 @@
+package com.example.wuzp.secondworld.database;
+
+import com.apkfuns.logutils.LogUtils;
+
+/**
+ * Created by wuzp on 2017/4/16.
+ */
+public class HSDBHelper {
+    //this is file just to express some db file.just to test for git server and some git command
+
+    public static void show(String msg) {
+        LogUtils.e("tag",msg);
+    }
+
+    public static void show(String tag,String msg){
+        LogUtils.e(tag,msg);
+    }
+
+}
