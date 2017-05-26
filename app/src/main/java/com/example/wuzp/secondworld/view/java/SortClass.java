b8a27fd09e7581964a6078f04c297f6978cd54fa
wzp09tjlg <wzp09tjlg@163.com>
2017-05-26 14:45:59 +0800

    1.add besier demo 2.add some java tec point

---
+++ app/src/main/java/com/example/wuzp/secondworld/view/java/SortClass.java
@@ -0,0 +1,10 @@
+package com.example.wuzp.secondworld.view.java;
+
+/**
+ * Created by wuzp on 2017/5/26.
+ * 一般算法：快速排序  堆排序 归并排序 shell排序 插入排序 选择排序 冒泡排序
+ * 找时间把这些算法都再熟悉一遍 然后使用代码实现出来
+ * 以前不太理解排序的速度，但是在成长之后 发现很多的东西在成长之后 根本不算什么，只是经验而已
+ */
+public class SortClass {
+}
